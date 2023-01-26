912c47625d40e117be2cbf764eba252b48b6e15c
Daniel-xsy <1491387884@qq.com>
2023-01-26 12:29:45 +0800

    update

--- zoo/BEVDet/mmdet3d/core/hook/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .ema import MEGVIIEMAHook
-from .utils import is_parallel
-from .sequentialsontrol import SequentialControlHook
-
-__all__ = ['MEGVIIEMAHook', 'is_parallel', 'SequentialControlHook']
