8f120285ea7c41ece66fa746f2b3561cbd1c7e6d
Daniel-xsy <1491387884@qq.com>
2023-02-05 21:23:50 +0800

    Merge branch 'master' of https://github.com/Daniel-xsy/RoboDet

--- zoo/BEVFormer/tools/dist_test.sh
+++ zoo/BEVFormer/tools/dist_test.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-CONFIG=/nvme/konglingdong/models/RoboDet/zoo/BEVFormer/projects/configs/robust_test/bevformer_base.py
+CONFIG=/nvme/konglingdong/models/RoboDet/zoo/BEVFormer/projects/configs/bevformer/bevformer_base.py
 CHECKPOINT=/nvme/konglingdong/models/RoboDet/models/BEVFormer/bevformer_r101_dcn_24ep.pth
-GPUS=1
+GPUS=8
 PORT=${PORT:-29503}
 
 PYTHONPATH="/nvme/konglingdong/models/RoboDet/zoo/BEVFormer":$PYTHONPATH \
-python -m debugpy --listen 5677 --wait-for-client -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4} --eval bbox
+    # -m debugpy --listen 5677 --wait-for-client 
