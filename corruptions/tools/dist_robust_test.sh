3e54d81e3e0a7d135d8faa57fe786db44127df4e
Daniel-xsy <1491387884@qq.com>
2023-02-13 15:53:54 +0800

    update

--- corruptions/tools/dist_robust_test.sh
+++ corruptions/tools/dist_robust_test.sh
@@ -7,4 +7,4 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/robust_test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
+    $(dirname "$0")/robust_test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4} --eval bbox \
