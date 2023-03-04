ba26246521a122094814fbdf0af63829297a24c3
Daniel-xsy <1491387884@qq.com>
2023-03-04 23:13:02 +0800

    Update script

--- corruptions/tools/generate_dataset.sh
+++ corruptions/tools/generate_dataset.sh
@@ -3,5 +3,4 @@
 CONFIG=$1
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m debugpy --listen 5678 --wait-for-client $(dirname "$0")/generate_dataset.py \
-$CONFIG \
+python $(dirname "$0")/generate_dataset.py $CONFIG
