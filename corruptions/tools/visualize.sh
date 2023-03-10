80ca2f61190f09880f5ea3c5709fbb25392046f1
Shaoyuan Xie <66255889+Daniel-xsy@users.noreply.github.com>
2023-03-10 16:06:29 +0800

    Delete visualize.sh

--- corruptions/tools/visualize.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-CONFIG=/nvme/konglingdong/models/RoboDet/corruptions/project/config/nuscenes_c_sample.py
-
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python ./tools/visual_tools/nuscenes_c_sample.py \
-${CONFIG}
-# -m debugpy --listen 5678 --wait-for-client 
