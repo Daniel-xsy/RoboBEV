aa465b4960ffbd6a8e648b5de0f2fcfec755a6de
Daniel-xsy <1491387884@qq.com>
2023-03-11 16:18:40 +0800

    Update custom loading

--- corruptions/project/mmdet3d_plugin/datasets/pipelines/__init__.py
+++ corruptions/project/mmdet3d_plugin/datasets/pipelines/__init__.py
@@ -2,9 +2,9 @@ from .custom_transform_3d import (
     PadMultiViewImage, NormalizeMultiviewImage, 
     PhotoMetricDistortionMultiViewImage, CustomCollect3D, RandomScaleImageMultiViewImage)
 from .custom_formating import CustomDefaultFormatBundle3D
-from .custom_loading import Custom_LoadMultiViewImageFromFiles
+from .custom_loading import Custom_LoadMultiViewImageFromFiles, Custom_LoadImageFromFileMono3D, Custom_LoadImageFromFile
 __all__ = [
     'PadMultiViewImage', 'NormalizeMultiviewImage', 
     'PhotoMetricDistortionMultiViewImage', 'CustomDefaultFormatBundle3D', 'CustomCollect3D', 'RandomScaleImageMultiViewImage',
-    'Custom_LoadMultiViewImageFromFiles'
+    'Custom_LoadMultiViewImageFromFiles', 'Custom_LoadImageFromFileMono3D', 'Custom_LoadImageFromFile'
 ]
