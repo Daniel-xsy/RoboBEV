bd5e5e0e4fe8f3821cd915de3ca0664fb345c443
Daniel-xsy <1491387884@qq.com>
2023-04-25 19:42:34 +0800

    Add monocular uda nuscenes dataset

--- uda/projects/mmdet3d_plugin/datasets/__init__.py
+++ uda/projects/mmdet3d_plugin/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .uda_nuscenes import UDANuScenesDataset
+from .uda_nuscenes_mono import UDANuScenesMonoDataset
 
-__all__ = ['UDANuScenesDataset']
+__all__ = ['UDANuScenesDataset', 'UDANuScenesMonoDataset']
